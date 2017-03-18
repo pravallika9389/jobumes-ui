fc9c99488fa3b72da00b9d5327461b8255b58556
Pradeep Kumar R <Pradeep Kumar R>
2017-03-18 13:51:59 +0530

    file upload

--- src/main/webapp/resource/js/custom/employee/controllers/employeeDashboardController.js
+++ src/main/webapp/resource/js/custom/employee/controllers/employeeDashboardController.js
@@ -1,4 +1,4 @@
-app.controller('employeeDashboardController', function($scope, $location, $rootScope) {
+app.controller('employeeDashboardController', function($scope, $location, $rootScope, $http, fileUpload) {
 	//$scope.message = 'Contact us! JK. This is just a demo.';
 	var accessData = angular.fromJson(window.localStorage['userObj']);
 	var returnData = angular.fromJson(window.localStorage['userDetailsObj']);
@@ -23,4 +23,86 @@ app.controller('employeeDashboardController', function($scope, $location, $rootS
 		$("#footersection").hide();
 		$(".hideclass").hide();
 	}
+
+	$scope.data = {
+	  "url": "http://183.82.1.143:9058/jobumes/resumes/Arun.docx",
+	  "name" : "Aru CV 1"
+	}
+
+	$scope.uploadFileLink = function(){
+		var valuesToBasic = 'Basic ' + btoa(accessData.userName + ':' + accessData.pass);
+		if($scope.resumeLink){
+			var res = $http({
+				method: 'POST',
+				url: 'http://183.82.1.143:9060/resumes',
+				headers: {'Authorization': valuesToBasic},
+				data: $scope.data
+			});
+
+			res.success(function(data, status, headers, config) {
+				console.log(data);
+				if (status == 200) {
+					alert("File Uploaded Successfully");
+				}else {
+					alert("Please check the File link");
+				}
+			})
+			res.error(function(data, status, headers, config) {
+					console.log(data);
+					alert("Server Error!");
+			});
+		}else {
+			alert("Please give a URL of a file");
+		}
+	}
+
+	$scope.uploadFile = function(){
+     var file = $scope.myFile;
+
+     console.log('file is ' );
+     console.dir(file);
+
+     var uploadUrl = "/resumes";
+     fileUpload.uploadFileToUrl(file, uploadUrl);
+  };
 });
+
+app.directive('fileModel', ['$parse', function ($parse) {
+	return {
+		 restrict: 'A',
+		 link: function(scope, element, attrs) {
+				var model = $parse(attrs.fileModel);
+				var modelSetter = model.assign;
+
+				element.bind('change', function(){
+					 scope.$apply(function(){
+							modelSetter(scope, element[0].files[0]);
+					 });
+				});
+		 }
+	};
+}]);
+
+app.service('fileUpload', ['$http', function ($http) {
+
+ var accessData = angular.fromJson(window.localStorage['userObj']);
+
+	this.uploadFileToUrl = function(file, uploadUrl){
+		uploadUrl = 'http://183.82.1.143:9060/resumes';
+		 var fd = new FormData();
+		 fd.append('file', file);
+		//  user details
+		 var valuesToBasic = 'Basic ' + btoa(accessData.userName + ':' + accessData.pass);
+
+		 $http.post(uploadUrl, fd, {
+				// transformRequest: angular.identity,
+				headers: {'Authorization': valuesToBasic}
+		 })
+
+		 .success(function(){
+		 })
+
+		 .error(function(){
+		 });
+	}
+}]);
